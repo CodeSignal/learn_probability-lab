2f928982297e801a33186cb64f737f79e837d9fd
diegochine <chine.diego@gmail.com>
2025-12-22 12:25:45 +0100

    Fix help content missing in prod deployment

--- client/src/shell/help.js
+++ client/src/shell/help.js
@@ -1,10 +1,11 @@
 // Help modal initialization
 // Loads help content and sets up the help modal using Design System Modal component
 
+import helpContentHtml from '../../help-content.html?raw';
+
 export async function initializeHelpModal(Modal, setStatus) {
   try {
-    const response = await fetch('./help-content.html');
-    const helpContent = await response.text();
+    const helpContent = helpContentHtml;
 
     // Create help modal using design system Modal component
     const helpModal = Modal.createHelpModal({
